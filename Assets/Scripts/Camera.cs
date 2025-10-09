89718054bae7161bfe9e29a8e66fdfa7a891eac8
JacobBMitchell <mattdamionoriginal@gmail.com>
2025-10-08 21:20:00 -0500

    Camera basic Done

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector2 motion;
     void Start()
     {
         Cursor.visible = false;
@@ -11,8 +12,10 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Input.mousePosition;
-        
+        //Turn mouse movement into camera rotation
+        motion.x += Input.GetAxis("Mouse X");
+        motion.y += Input.GetAxis("Mouse Y");
+        transform.localRotation = Quaternion.Euler(-motion.y, motion.x, 0);
     }
     
 }
